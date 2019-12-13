622ff2196a534a2f8b06963c2c28cb1332568665
470538922 <470538922@qq.com>
2019-12-13 13:35:54 +0800

    修改BUG

--- src/components/global/Global.vue
+++ src/components/global/Global.vue
@@ -1,7 +1,7 @@
 <script>
-const apiSrc = "http://192.168.1.104:9880";
-const imgPath = "http://192.168.1.104:8080/";
-const apiImg = "http://192.168.1.104:9886/upload";
+const apiSrc = "http://117.78.4.95:8015";
+const imgPath = "http://117.78.4.95:8020/";
+const apiImg = "http://117.78.4.95:8019/upload";
 
 // const apiSrc = "http://118.25.217.48:9880";
 // const imgPath = "http://118.25.217.48:8080/";
@@ -11,8 +11,8 @@ const apiImg = "http://192.168.1.104:9886/upload";
 // const imgPath = "http://118.25.217.48:8080/";
 // const apiImg = "http://118.25.217.48:9886/upload";
 export default {
-	apiSrc,
-	apiImg,
-	imgPath
+  apiSrc,
+  apiImg,
+  imgPath
 };
 </script>
